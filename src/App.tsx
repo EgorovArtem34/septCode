9f4c582ab2caaf1bd9ccba3a9471f912eae64136
Artem Egorov <artegtor@gmail.com>
2023-09-02 13:18:52 +0500

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <div className="header" />
+      <main className="main">
+        <div className="container">
+          <span>Старт</span>
+        </div>
+      </main>
+    </>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  // devtool: "inline-source-map",
+  watchOptions: {
+    poll: 1000, // Check for changes every second
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: "./dist",
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: 'asset/resource'
+    },
+    {
+      test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+      type: 'asset/inline',
+    },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+  ],
+};
